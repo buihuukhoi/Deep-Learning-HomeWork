088b7fa9005bd8cf363a19bb5950e02c538174a2
buihuukhoi <khoibuihuu@gmail.com>
2019-11-21 00:52:29 +0800

    load data using tensorflow and train cnn model using tensorflow.keras

--- HW2/Helper/LoadMNISTData.py
+++ HW2/Helper/LoadMNISTData.py
@@ -1,25 +1,35 @@
 from tensorflow.examples.tutorials.mnist import input_data
+import numpy as np
 
-def load_data(mode='train'):
+def load_data(mode='train', loss='categorical_crossentropy'):
     """
     Function to load the MNIST data from tensorflow
     :param mode: train of test
+    :param loss: loss parameter when compile model
     :return: images and labels
     """
-    mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
+    mnist = None
+    if loss == 'categorical_crossentropy':  # label is one_hot_vectors
+        mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
+    elif loss == 'sparse_categorical_crossentropy':  # label is integers
+        mnist = input_data.read_data_sets("MNIST_data", one_hot=False)
     if mode == 'train':
-        x_train, y_train = mnist.train.images, mnist.train.labels
-        x_validation, y_validation = mnist.validation.images, mnist.validation.labels
+        x_train, y_train = mnist.train.images, mnist.train.labels  # 55000x784 55000x10
+        x_validation, y_validation = mnist.validation.images, mnist.validation.labels  # 5000x784, 5000x10
+        x_train = reformat(x_train)
+        x_validation = reformat(x_validation)
         return x_train, y_train, x_validation, y_validation
     elif mode == 'test':
         x_test, y_test = mnist.test.images, mnist.test.labels
+        x_test = reformat(x_test)
         return x_test, y_test
 
-    def reshape(x, y):
-        """
-        Function to reformat the data then it can be used by convolutional layers
-        :param x: input array of images
-        :param y: corresponding labels of images
-        :return: reshaped input images and labels
-        """
-        return None
+def reformat(x, num_of_channel=1):
+    """
+    Function to reformat the data then it can be used by convolutional layers
+    :param x: input array of images
+    :return: reshaped input images
+    """
+    img_size = int(np.sqrt(len(x[0])))
+    x = x.reshape((-1, img_size, img_size, num_of_channel)).astype(np.float32)
+    return x
--- HW2/main.py
+++ HW2/main.py
@@ -1,5 +1,30 @@
+from tensorflow.keras import datasets, layers, models
 from Helper import LoadMNISTData as MNIST
 
 # load data
-train_data, train_label, validation_data, validation_label = MNIST.load_data('train')  # 55000x784, 55000x10, 5000x784, 5000x10
-test_data, test_label = MNIST.load_data('test')  # 10000x784, 10000x10
+x_train, y_train, x_validation, y_validation = MNIST.load_data('train')  # 55000x28x28x1, 55000x10, 5000x28x28x1, 5000x10
+x_test, y_test = MNIST.load_data('test')  # 10000x28x28x1, 10000x10
+
+# create the network layers
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1), padding='SAME'))
+model.add(layers.MaxPooling2D(2, 2))
+model.add(layers.Conv2D(64, (3, 3), activation='relu', padding='SAME'))
+model.add(layers.MaxPooling2D(2, 2))
+model.add(layers.Conv2D(128, (3, 3), activation='relu', padding='SAME'))
+model.add(layers.MaxPooling2D(2, 2))
+model.add(layers.Flatten())
+model.add(layers.Dense(128, activation="relu"))
+#model.add(layers.Dropout(0.2))  # Dropout to combat overfitting in neural networks
+model.add(layers.Dense(10, activation="softmax"))
+#model.build()
+model.summary()
+
+#train the model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+history = model.fit(x_train, y_train, epochs=1, validation_data=(x_validation, y_validation))
+#evaluate model
+loss_result, accuracy_result = model.evaluate(x_test, y_test)
+print('Testing Loss is {}'.format(loss_result))
+print('Testing Accuracy is {}'.format(accuracy_result))
+
